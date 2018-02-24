ad8327e13177f2f7d8f765c477c8ab137e21f1fe
nao-sph <naoki.nagai@icloud.com>
2018-02-24 18:00:57 +0900

    fix port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -19,6 +19,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
